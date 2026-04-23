2c864b3968c11cf9d635777a9873d964600f5e88
Alequa <alexchanel@gmail.com>
2026-04-23 21:22:13 +0500

    M1 finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.material)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
